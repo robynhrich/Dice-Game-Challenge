7034d0acc1fd57fc989a8d110d4bca651626c113
Robyn H Richardson <robynhrich@icloud.com>
2024-01-27 23:59:36 -0500

    Created function to determine winner upon refresh

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ document
 document
   .querySelector(".img2")
   .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+function nameChampion() {
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+}
+
+nameChampion();
